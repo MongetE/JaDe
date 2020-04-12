1fa8b1d8cbffd57813dc437c3f10f8dc7222e4ed
RhodriiMawr <rhodrii.mawr@gmail.com>
2020-04-12 17:53:01 +0200

    Add docstring

--- JaDe/jade/preprocessor.py
+++ JaDe/jade/preprocessor.py
@@ -2,9 +2,11 @@ import json
 import os
 import pathlib
 import re
+import spacy
 
 
 DIR = pathlib.Path('data/annotated_poems')
+nlp = spacy.load('en_core_web_sm')
 
 
 def enj_marker(line):
@@ -33,11 +35,32 @@ def enj_marker(line):
     return marked
 
 
-def cleaner(content):
+def build_dict(content):
     """
-    :param content: str, numbered + annotated poem
-    :return: a dict organized according to line pairs. Keys include the lines, the annotations and whether it is an
-            an enjambment or not
+        Build a json object.
+
+        Ideally, this dictionary should also contains the sentence(s) found
+        within the line pair and only the section(s) of these sentence(s)
+        corresponding to the line pair.
+
+        Parameters
+        ----------
+            content: str
+                An annotated poem
+
+        Returns
+        -------
+            json_dict: dict
+                A list of dictionaries in which the following keys are found:
+                * nbPair is the id of the line pair
+                * text is the marked version of the line pair
+                * annot are the annotations given for the line pair
+                * isEnj is a bool, False if there is no enjambment, True otherwise
+
+            poem: str
+                May be removed.
+                The poem with a mark at the end of each line, indicating
+                whether the line is end-stopped (&&) or enjambed (%%).
     """
     json_dict = []
 
@@ -53,7 +76,8 @@ def cleaner(content):
     for i in range(len(tmp)):
         tmp_dict = dict()
 
-        tmp_dict['nbPair'], tmp_dict['text'], tmp_dict['annot'] = tmp[i][0], pairs[i], tmp[i][1]
+        tmp_dict['nbPair'], tmp_dict['annot'] = tmp[i][0], tmp[i][1]
+        tmp_dict['text'], tmp_dict['marked_text'] = re.sub('(%%|&&)', '', pairs[i]), pairs[i]
 
         if '%%' in pairs[i]:
             tmp_dict['isEnj'] = True
@@ -63,3 +87,83 @@ def cleaner(content):
         json_dict.append(tmp_dict)
 
     return json_dict, poem
+
+
+def get_sentences(poem):
+    # TODO: change list comprehensions by for loop to avoid looping three times on the same list
+    """
+        Take a poem and destructure it into sentences.
+
+        Paramaters
+        ----------
+            poem: str
+
+        Returns
+        -------
+            list
+             Three lists corresponding to the sentences, the tokens in
+             the sentences and the POS in the sentences.
+    """
+    doc = nlp(poem)
+    sp_sentences = [str(sent) for sent in doc.sents]
+    tokens_in_sent = [[str(token) for token in nlp(sentence)] for sentence in sp_sentences]
+    pos_in_sent = [[str(token.pos_) for token in nlp(sentence)] for sentence in sp_sentences]
+    tags_in_sent = [[str(token.tag_) for token in nlp(sentence)] for sentence in sp_sentences]
+
+    return sp_sentences, tokens_in_sent, pos_in_sent, tags_in_sent
+
+
+def reconstruct_poem(json_dict):
+    """
+        Reconstruct the poem from the json representation of that poem.
+
+        Parameters
+        ----------
+        json_dict: dict
+            A json object representing a poem.
+
+        Returns
+        -------
+            str
+                The poem
+            list
+                Two lists: one containing the last word of enjambed lines
+                and the other, the last word of end-stopped lines.
+    """
+    poem = ""
+    last_words_enj = []
+    last_words_end = []
+    for item in json_dict:
+        line_pair = item.get('marked_text')
+        if item == json_dict[0]:
+            poem += line_pair.lower().split('%%')[0] + line_pair.lower().split('%%')[1]
+            last_words_enj.append(re.search(r'\w*(?=%%)', line_pair).group(0))
+        elif '&&' in line_pair:
+            poem += line_pair.lower().split('&&')[0] + line_pair.lower().split('&&')[1]
+            last_words_end.append(re.search(r'\w*(?=\W&&)', line_pair).group(0))
+        else:
+            if "&&" in line_pair:
+                poem += line_pair.lower().split('&&')[1]
+                last_words_end.append(re.search(r'\w*(?=\W&&)', line_pair).group(0))
+            else:
+                poem += line_pair.lower().split('%%')[1]
+                last_words_enj.append(re.search(r'\w*(?=%%)', line_pair).group(0))
+
+        return poem, last_words_enj, last_words_end
+
+
+def main():
+    for file in DIR.iterdir():
+        with open(str(file), 'r', encoding='utf-8') as curfile:
+            filename = str(file)[21:-4].replace(',', '').replace('\'', '').replace('.', '').replace(' ', '_')\
+                .replace(':', '').replace('?', '').lower()
+            content = curfile.read()
+            json_dict, poem = build_dict(content)
+            print(json_dict)
+            reconstructed, last_words_enj, last_words_end = reconstruct_poem(json_dict)
+            sentences, tokens_in_sent, pos_in_sent, tags_in_sent = get_sentences(reconstructed)
+            # writer(json_dict, filename, poem)
+
+
+if __name__ == '__main__':
+    main()
