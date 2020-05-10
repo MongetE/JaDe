4429b0d87c1b679bfb3c66d32717a54488dc172f
EMonget <rhodrii.mawr@gmail.com>
2020-05-10 15:07:57 +0200

    Add scklearn metrics

--- JaDe/jade/eval.py
+++ JaDe/jade/eval.py
@@ -2,14 +2,28 @@ import os
 import re
 import sys
 import pathlib
+from sklearn.metrics import classification_report
 
 ANNOT_DIR = r'JaDe/resources/annotated_poems'
 DETECTED_DIR = r'JaDe/resources/detected'
 DETECTED_TYPES = [
-                    'pb_noun_prep', 'pb_noun_noun', 'cross_clause', 'pb_verb_adv',
-                    'pb_det_noun', 'pb_v_chain', 'pb_adj_noun', 'pb_adj_adj', 
+                    'pb_noun_prep', 'pb_noun_noun', 'cc_cross_clause', 'pb_verb_adv',
+                    'pb_det_noun', 'pb_v_chain', 'pb_noun_adj', 'pb_adj_adj', 
                     'pb_adv_adv', 'pb_adj_adv'
                 ]
+
+DET_NOUN = 'pb_det_noun'
+NOUN_NOUN = 'pb_noun_noun'
+ADJ_NOUN = 'pb_noun_adj'
+ADJ_ADJ = 'pb_adj_adj'
+NOUN_PREP = 'pb_noun_prep'
+CROSS = 'cc_cross_clause'
+V_CHAIN = 'pb_v_chain'
+ADV_ADV = 'pb_adv_adv'
+VERB_ADV = 'pb_verb_adv'
+ADJ_ADV = 'pb_adj_adv'
+END_STOPPED = ''
+
 
 def get_manual_annotations(file): 
 
@@ -56,9 +70,13 @@ if __name__ == "__main__":
             if poem == curr_poem_name:
                 annotations[poem].append(get_detected_annotations(filename))
 
+    
     for poem, enjambments in annotations.items():
+        print(poem)
         manual_annotations = enjambments[0]
         automatic_annotations = enjambments[1]
-        for i in range(len(manual_annotations)):
-            if len(automatic_annotations[i]) == len(manual_annotations[i]):
-                print('detected: ', automatic_annotations[i], '\tmanual: ', manual_annotations[i])
+        print(classification_report(manual_annotations, automatic_annotations, digits=3))
+        print()
+        # for i in range(len(manual_annotations)):
+        #     if len(automatic_annotations[i]) == len(manual_annotations[i]):
+        #         print('detected: ', automatic_annotations[i], '\tmanual: ', manual_annotations[i])
